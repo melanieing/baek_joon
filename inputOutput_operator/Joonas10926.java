1eff16c330407e2eb04b77e525635ced32a82cf1
melanieing <91274798+melanieing@users.noreply.github.com>
2022-04-11 00:02:34 +0900

    Day003

---
+++ inputOutput_operator/Joonas10926.java
@@ -0,0 +1,16 @@
+package inOutOperator;
+
+import java.util.Scanner;
+
+public class Joonas10926 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.nextLine();
+		sc.close();
+		System.out.println(id + "??!");
+	}
+
+}
