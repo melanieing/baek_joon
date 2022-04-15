90ac6cff604ca5c2b62c6b26b8be6247d3659e6a
melanieing <91274798+melanieing@users.noreply.github.com>
2022-04-15 13:40:05 +0900

    Day005

---
+++ loop/SumMinus10950.java
@@ -0,0 +1,23 @@
+package loop;
+
+import java.util.Scanner;
+
+public class SumMinus10950 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		int[] nums = new int[testCase*2];
+		int[] sums = new int[testCase];
+		
+		for (int i = 0; i < testCase*2; i++) {
+			nums[i] = sc.nextInt();
+		}
+		for (int i = 0; i < testCase; i++) {
+			sums[i] = nums[i*2] + nums[i*2+1];
+			System.out.println(sums[i]);
+		}
+		sc.close();
+	}
+}
