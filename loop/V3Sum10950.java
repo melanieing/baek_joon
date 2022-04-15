7f488160038e0be91075f798d7d381d266869bac
melanieing <91274798+melanieing@users.noreply.github.com>
2022-04-15 13:42:37 +0900

    Day005

--- loop/V3Sum10950.java
+++ loop/V3Sum10950.java
@@ -2,7 +2,7 @@ package loop;
 
 import java.util.Scanner;
 
-public class SumMinus10950 {
+public class V3Sum10950 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
