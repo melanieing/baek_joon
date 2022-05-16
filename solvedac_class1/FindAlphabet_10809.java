703e9fa1a3eac12d37866e8db4d62fed3b1a056d
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2022-05-16 16:28:41 +0900

    Add files via upload

---
+++ solvedac_class1/FindAlphabet_10809.java
@@ -0,0 +1,20 @@
+package solvedac;
+
+import java.util.Scanner;
+
+public class FindAlphabet_10809 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	
+		String word = sc.next();
+
+		for (char i = 'a'; i <= 'z'; i++) {
+			if (!word.contains(Character.toString(i))){
+				System.out.print(-1 + " ");
+				continue;
+			}
+			System.out.print(word.indexOf(i) + " ");
+		}
+	}
+}
