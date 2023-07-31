50d175fdbeba504aeb1ba7e438d8767d3961308b
Melanie Woo <91274798+melanieing@users.noreply.github.com>
2023-07-31 22:19:08 +0900

    15736. 청기 백기

---
+++ math/BlueWhite15736.java
@@ -0,0 +1,13 @@
+package integer;
+
+import java.util.Scanner;
+// 15736. 청기 백기
+public class BlueWhite15736 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println((int)Math.sqrt(n));
+    }
+}
